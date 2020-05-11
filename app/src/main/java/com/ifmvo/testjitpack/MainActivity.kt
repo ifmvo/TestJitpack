4b46ef202392280e064e78ecbf9320a4429263aa
Matthew_Chen <Matthew_Chen_1994@163.com>
2020-05-11 17:31:10 +0800

    多module情况下jitpack可行

--- app/src/main/java/com/ifmvo/testjitpack/MainActivity.kt
+++ app/src/main/java/com/ifmvo/testjitpack/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.ifmvo.testjitpack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ifmvo.testjitpack1.TestJitpack1
+import com.ifmvo.testjitpack2.TestJitpack2
+import com.ifmvo.testjitpack3.TestJitpack3
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TestJitpack1.print1()
+        TestJitpack2.print2()
+        TestJitpack3.print3()
     }
 }
